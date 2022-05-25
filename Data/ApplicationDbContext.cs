ac6112f20f69ecce2d898736c4311137ad147472
Ibtissam Zerouali <ibtissamzerouali7@gmail.com>
2022-05-25 23:25:05 +0100

    CrudCategory

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using stock_management.Models;
+
+namespace stock_management.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
