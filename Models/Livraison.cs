42aef877c015d5a1eb533eb8c707341ce633b29b
YassineOujaa77 <yassineoujaa42@gmail.com>
2022-06-06 10:16:24 +0100

    Final Steps

---
+++ Models/Livraison.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace GestionDeStockMagasin.Models
+{
+    public class Livraison
+    {
+        [Key]
+        public int idLivraison { get; set; }
+  
+        public int idFacture { get; set; }
+        public Facture facture { get; set; }
+        public string nameCL { get; set; }
+
+        public string namePR { get; set; }
+        public string livreur { get; set; }
+
+    }
+}
